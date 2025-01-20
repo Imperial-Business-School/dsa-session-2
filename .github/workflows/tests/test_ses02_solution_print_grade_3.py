be77e4f8162e4655dbf49ed9ed1ebb2633493078
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-01-20 12:44:00 +0000

    feat: add autograding

---
+++ .github/workflows/tests/test_ses02_solution_print_grade_3.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'test_ses02_solution_print_grade_3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print_grade(50, 70, 50)
+          pass
+          """,
+          'hidden': False
+        }
+      ],
+      'scored': True,
+      'setup': r"""
+      >>> from ses02 import *
+      """,
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
